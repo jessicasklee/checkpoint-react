08da9485fd045771fa212de5531cdcee60737b1b
Jessica Lee <jsurkilee@gmail.com>
2019-02-28 14:17:30 -0500

    init commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import "../Contact/Contact"
+import { Link, Route } from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contacts: props.contacts
+    }
   }
+
+  render() {
+    return(
+      <Router>
+      <div className="App">
+      <header>
+        <nav>
+          <Link to="/">Contact List</Link>
+          <Link to="/new-contact">New Contacts</Link>
+        </nav>
+          <Route path="/" component={ ContactList }/>
+          <Route path="/new-contact" component={ NewContact }/>
+      </header>
+        <p>app</p>
+      </div>
+      </Router>
+    )}
 }
 
 export default App;
